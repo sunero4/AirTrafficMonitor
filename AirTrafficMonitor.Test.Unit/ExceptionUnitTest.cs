8abe28f25867c32f3c6b7887e8aa4bd9d7bc5d74
DanielDKnudsen <daniel.d.knudsen@gmail.com>
2018-04-10 08:58:27 +0200

    Exceptiontest lavet

---
+++ AirTrafficMonitor.Test.Unit/ExceptionUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitor.Exceptions;
+using NUnit.Framework;
+
+namespace AirTrafficMonitor.Test.Unit
+{
+    [TestFixture]
+    class ExceptionUnitTest
+    {
+        private TransponderDataConversionException _uut;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new TransponderDataConversionException();
+        }
+
+        [Test]
+        public void TestOutputException_ThrowException_CorrectStringIsContainedInException()
+        {
+           Assert.That(_uut.Message == "The input was invalid. Check amount of separators.");
+        }
+    }
+}
